67fde762326eb8f70177dc64ff7c206eb5a71fcb
TinyXI <105007573+TinyAII@users.noreply.github.com>
2026-08-27 16:47:03 +0800

    Skills v1.0.0 - add src/main/java/nl/tinyaii/skills/gui/SkillsHolder.java

---
+++ src/main/java/nl/tinyaii/skills/gui/SkillsHolder.java
@@ -0,0 +1,12 @@
+package nl.tinyaii.skills.gui;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+/**
+ * 技能面板 GUI 标识。
+ */
+public class SkillsHolder implements InventoryHolder {
+    @Override
+    public Inventory getInventory() { return null; }
+}
